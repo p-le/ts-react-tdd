5c052b9527731d3301901a8d2acf68e84953212a
p-le <p-le@dimage.co.jp>
2017-07-27 14:28:27 +0900

    add jest, enzyme test

--- src/components/Hello.spec.ts
+++
@@ -1,2 +0,0 @@
-import * as React from 'react';
-import * as Enzyme from 'enzyme';
---
+++ src/components/Hello.spec.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import { Hello } from './Hello';
+import { shallow } from 'enzyme';
+
+describe('<Hello />', () => {
+  it('should renders properly', () => {
+    const wrapper = shallow(<Hello compiler='Typescript' framework='React' />);
+    expect(wrapper.text()).toEqual('Hello from Typescript and React');
+  });
+});
